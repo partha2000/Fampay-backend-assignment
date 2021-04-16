9703fa5d51b747d2647e58b23ffbf576bf9d02da
parhta2000 <parthaprateem2000@gmail.com>
2021-04-16 12:26:47 +0530

    modified and registered model to admin

--- youtube_api/admin.py
+++ youtube_api/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import videoData
 # Register your models here.
+
+admin.site.register(videoData)
--- youtube_api/models.py
+++ youtube_api/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class videoData(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length= 50)
     description = models.TextField()
-    channel_name = models.CharField()
+    channel_name = models.CharField(max_length= 20)
     pub_date_time = models.DateTimeField()
     thumbnailURL = models.URLField()
