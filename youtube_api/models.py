dba3cdb52147ac075dec81819b3782299ceae3ba
parhta2000 <parthaprateem2000@gmail.com>
2021-04-16 12:17:20 +0530

    created new model for the youtube data

--- youtube_api/models.py
+++ youtube_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class videoData(models.Model):
+    title = models.CharField()
+    description = models.TextField()
+    pub_date_time = models.DateTimeField()
+    thumbnailURL = models.URLField()
