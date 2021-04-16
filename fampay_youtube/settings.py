8493b3b23dd8e5ec2092cb94e53a1f618e8fcbcf
parhta2000 <parthaprateem2000@gmail.com>
2021-04-16 12:32:29 +0530

    setup cron to manage background tasks

--- fampay_youtube/settings.py
+++ fampay_youtube/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'django_crontab',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,3 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRONJOBS = [
+    ('*/5 * * * *', 'youtube_api.cron.my_scheduled_job')
+]
