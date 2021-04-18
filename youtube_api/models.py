b5a221ba545a9a1b62922eca54bf74651550a311
parhta2000 <parthaprateem2000@gmail.com>
2021-04-18 20:23:17 +0530

    put the API call in try except block

--- youtube_api/models.py
+++ youtube_api/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.utils.timezone import now
 
 # Model class for the video data objects
+# videoID being unique is the primary key 
+# for the table
 class videoData(models.Model):
     videoID = models.CharField(max_length=50,primary_key=True,editable=False)
     title = models.CharField(max_length= 50)
--- youtube_api/tasks.py
+++ youtube_api/tasks.py
@@ -21,20 +21,22 @@ def periodic_task():
             type="video",
             q="valorant"
     )
+    try:
+        response = search_videos.execute()  
+    except:
+        print("Service unavailable at the moment or the API quota has been exhausted")
 
-    response = search_videos.execute() 
-
-    for i in response['items']:
-        p = videoData(
-                        videoID = i['id']['videoId'],
-                        title=i['snippet']['title'],
-                        description=i['snippet']['description'],
-                        channel_name=i['snippet']['channelTitle'],
-                        pub_date_time=dateparse.parse_datetime(i['snippet']['publishedAt']),
-                        thumbnailURL=i['snippet']['thumbnails']['default']['url'])
-        p.save()
-
-    print("Youtube data saved to database")
+    if response != None:
+        for i in response['items']:
+            p = videoData(
+                            videoID = i['id']['videoId'],
+                            title=i['snippet']['title'],
+                            description=i['snippet']['description'],
+                            channel_name=i['snippet']['channelTitle'],
+                            pub_date_time=dateparse.parse_datetime(i['snippet']['publishedAt']),
+                            thumbnailURL=i['snippet']['thumbnails']['default']['url'])
+            p.save()
+        print("Youtube data saved to database")
 
     for row in videoData.objects.all().reverse():
         if videoData.objects.filter(videoID=row.videoID).count() > 1:
