12fb160b9673e0c3e4b3d4eec9293642704325b3
parhta2000 <parthaprateem2000@gmail.com>
2021-04-18 17:15:19 +0530

    added pagination to the GET method

--- youtube_api/views.py
+++ youtube_api/views.py
@@ -7,6 +7,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import generics
 from django.http import JsonResponse
+from rest_framework.settings import api_settings
 
 from .serializers import videoDataSerializer
 from .models import videoData
@@ -14,6 +15,7 @@ from .models import videoData
 class videoDataView(mixins.ListModelMixin,
     generics.GenericAPIView,
     mixins.CreateModelMixin):
+    pagination_class = api_settings.DEFAULT_PAGINATION_CLASS ## This will auto paginate the repsonse
 
     serializer_class = videoDataSerializer
     queryset = videoData.objects.all().order_by('-pub_date_time')
